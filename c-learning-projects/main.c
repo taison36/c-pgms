085cd60e86059957125968c26d72f321a89da287
prince <tihon.korol77@gmail.com>
2025-01-19 14:28:40 +0100

    Initial Commit

---
+++ c-learning-projects/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c-learning-projects
+//
+//  Created by Tykhon Korol on 19.01.25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
