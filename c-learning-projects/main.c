5bc05d6d05d491fe40692f1eb10f41ace698ed17
prince <tihon.korol77@gmail.com>
2025-01-21 22:33:56 +0100

    first commit

--- c-learning-projects/main.c
+++ c-learning-projects/main.c
@@ -10,5 +10,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    printf("irgendwas gemacht");
+    printf("irgendwas gemacht");
+    printf("irgendwas gemacht");
     return 0;
 }
